3e9be048b74eaaea4707657630a2288414b4f5f8
TrufasBook <gabriel.vegapp@gmail.com>
2025-11-06 12:58:55 -0300

    v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pytrufas',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['pytrufas'],
     install_requires=[],
     author='Trufas',
 )
